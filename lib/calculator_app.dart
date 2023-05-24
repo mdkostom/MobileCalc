64a0ce3c25d9784dc39fa4e09bdc3ab8950cc394
mdkostom <mark.kostomahha@eek.ee>
2023-05-24 18:46:36 +0300

    Task 2 - Basic calculator UI

---
+++ lib/calculator_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'calculator_screen.dart';
+
+// Defines the MyApp class
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: Calculator(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'calculator_app.dart';
+
+// Responsible for starting the app
+void main() {
+  runApp(MyApp());
+}
